e44122c7aad4c73ab9548224bb6c974607041cb0
Giuseppe Albrizio <g.albrizio@gmail.com>
2022-02-21 16:57:59 +0100

    setup and fix jest test init
    setup and fix jest test init

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,12 +1,17 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import app from '../app';
 
-export const isNumber = (n: any) => {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+beforeAll(async () => {
+  await mongoose.connect(<string>process.env.MONGO_URI_TEST);
+});
 
 describe('GET /api/v1/servicename', () => {
   test('should return 200 OK', () => {
-    return request(app).get('/api/v1/servicename').expect(200);
+    return request(app).get('/api/v1/rest_service_name').expect(200);
   });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
 });
